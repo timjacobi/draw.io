120290a9801605108b8d1b6b993ea565c3d2e97a
Tim Jacobi <info@tim-jacobi.com>
2017-01-27 12:04:36 +0000

    BoxClient first drop

---
+++ war/js/diagramly/BoxClient.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2006-2016, JGraph Ltd
+ * Copyright (c) 2006-2016, Gaudenz Alder
+ */
+/**
+ * Constructs a new point for the optional x and y coordinates. If no
+ * coordinates are given, then the default values for <x> and <y> are used.
+ * @constructor
+ * @class Implements a basic 2D point. Known subclassers = {@link mxRectangle}.
+ * @param {number} x X-coordinate of the point.
+ * @param {number} y Y-coordinate of the point.
+ */
+BoxClient = function(editorUi)
+{
+	mxEventSource.call(this);
+	
+	/**
+	 * Holds a reference to the UI. Needed for the sharing client.
+	 */
+	this.ui = editorUi;
+};
+
+// Extends mxEventSource
+mxUtils.extend(BoxClient, mxEventSource);
+
+// TODO: add missing implementation
