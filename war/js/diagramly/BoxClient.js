7cffb1a2ac7360cdec816f9859b0e0c368e7aa6c
Tim Jacobi <info@tim-jacobi.com>
2017-01-27 20:47:41 +0000

    start implementing BoxClient

--- war/js/diagramly/BoxClient.js
+++ war/js/diagramly/BoxClient.js
@@ -24,3 +24,34 @@ BoxClient = function(editorUi)
 mxUtils.extend(BoxClient, mxEventSource);
 
 // BOXTODO: add missing implementation
+
+BoxClient.prototype.getUser = function(){
+	console.log("BOXTODO: Implement getUser");
+}
+
+BoxClient.prototype.pickFile = function(){
+	console.log("BOXTODO: Implement pickFile");
+	
+
+	var options = {
+		clientId: 'v4igy81pd6vexpn4ps0mnn5n4gog4wmk',
+		linkType: 'direct',
+		multiselect: false
+	};
+	
+	var boxSelect = new BoxSelect(options);
+
+	// Register a success callback handler
+	boxSelect.success(function(response) {
+		console.log(response);
+	});
+	// Register a cancel callback handler
+	boxSelect.cancel(function() {
+		console.log("The user clicked cancel or closed the popup");
+	});
+
+	// Opens up the file picker window
+	// Could use it to trigger a launch of the popup from your own button
+	// NOTE: Should be triggered on a user action
+	boxSelect.launchPopup();
+}
